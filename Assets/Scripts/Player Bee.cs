67a591054a46b05795924ecb90de8331b9c8c891
Matvey Volkov <whitewolf.mot185@gmail.com>
2023-12-01 02:55:43 +0300

    camera and controll are good now

--- Assets/Scripts/Player Bee.cs
+++ Assets/Scripts/Player Bee.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class BeeController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float rotationSpeed = 500f;
+    // movement control panels
+
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float turnSmoothTime = 0.1f;
+    [SerializeField] private float turnSmoothVelocity;
+
+    [SerializeField] private Transform cam;
+    [SerializeField] private CharacterController controller;
 
     private Rigidbody rb;
     private Animator animator;
@@ -17,29 +23,29 @@ public class BeeController : MonoBehaviour
 
     void FixedUpdate()
     {
-        MoveBee();
-        UpdateAnimations();
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized;
+        MoveBee(moveInput);
+        UpdateAnimations(moveInput);
     }
 
-    void MoveBee()
+    void MoveBee(Vector3 moveInput)
     {
-        Vector3 moveInput = new Vector3(-Input.GetAxis("Horizontal"), 0, -Input.GetAxis("Vertical"));
-
-        if (moveInput.magnitude > 0.1f)
+        if (moveInput.magnitude >= 0.1f)
         {
-            Quaternion rotation = Quaternion.LookRotation(moveInput, Vector3.up);
+            float targetAngle = Mathf.Atan2(moveInput.x, moveInput.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            transform.localRotation = Quaternion.Lerp(transform.localRotation, rotation, rotationSpeed * Time.deltaTime);
-
-            rb.velocity = transform.forward * moveSpeed;
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
         }
     }
 
 
-    void UpdateAnimations()
+    void UpdateAnimations(Vector3 moveInput)
     {
-        animator.SetBool("IsMoving", rb.velocity.magnitude > 0.1f);
-        animator.SetBool("IsRotating", rb.angularVelocity.magnitude > 0.1f);
+        animator.SetBool("IsMoving", moveInput.magnitude > 0.1f);
+        animator.SetBool("IsRotating", moveInput.magnitude > 0.1f);
         /*Debug.Log(animator.GetBool("IsMoving"));*/
     }
 }
